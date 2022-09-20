79d0b69bcb443ce7141f3631ff5ddd70f325166b
Jacinta Njuguna <102421883+jacintanjuguna@users.noreply.github.com>
2022-09-20 17:15:01 +0300

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+    <nav>
+        <img src ="../assets/airbnb-logo.png"  alt="" className="nav--logo"/>
+        </nav>
+    )
+}
