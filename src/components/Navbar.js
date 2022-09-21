9921a1e981c325937cbaab2f4fe113640b51702d
Jacinta Njuguna <102421883+jacintanjuguna@users.noreply.github.com>
2022-09-21 17:07:22 +0300

    Delete src directory

--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Navbar() {
-    return (
-    <nav>
-        <img src ="../assets/airbnb-logo.png"  alt="" className="nav--logo"/>
-        </nav>
-    )
-}
